34f99e82840d8bcedf12891c6d5e88032a384bd5
Jubaer Ahmad <ielahi61@gmail.com>
2025-09-03 00:18:54 +0600

    almost done with the project. the project is incomplete but  I want to submit it to move forward

--- index.js
+++ index.js
@@ -39,16 +39,149 @@ document.getElementById('heart9').addEventListener('click', function(){
 });
 
 
+let copy = document.getElementById('copy');
+let copyCount = parseInt(copy.textContent);
+document.getElementById('copy1').addEventListener('click', function(){
+    copyCount = copyCount + 1;
+    copy.textContent = copyCount;
+})
+document.getElementById('copy2').addEventListener('click', function(){
+    copyCount = copyCount + 1;
+    copy.textContent = copyCount;
+})
+document.getElementById('copy3').addEventListener('click', function(){
+    copyCount = copyCount + 1;
+    copy.textContent = copyCount;
+})
+document.getElementById('copy4').addEventListener('click', function(){
+    copyCount = copyCount + 1;
+    copy.textContent = copyCount;
+})
+document.getElementById('copy5').addEventListener('click', function(){
+    copyCount = copyCount + 1;
+    copy.textContent = copyCount;
+})
+document.getElementById('copy6').addEventListener('click', function(){
+    copyCount = copyCount + 1;
+    copy.textContent = copyCount;
+})
+document.getElementById('copy7').addEventListener('click', function(){
+    copyCount = copyCount + 1;
+    copy.textContent = copyCount;
+})
+document.getElementById('copy8').addEventListener('click', function(){
+    copyCount = copyCount + 1;
+    copy.textContent = copyCount;
+})
+document.getElementById('copy9').addEventListener('click', function(){
+    copyCount = copyCount + 1;
+    copy.textContent = copyCount;
+})
+
+
+
+
 
 let coinCount = document.getElementById('coin');
-
-document.getElementById('call-btn').addEventListener('click', function(){
-    let currentCoin = parseInt(coinCount.textContent)
-    if(coinCount.textContent >= 20){
-        alert('call');
-        coinCount.textContent = currentCoin - 20;
+let availableCoin = parseInt(coinCount.textContent);
+document.getElementById('call-btn1').addEventListener('click', function(){
+    
+    if(availableCoin >= 20){
+        alert("calling National Emergency Number 999");
+        availableCoin = availableCoin - 20;
+        coinCount.textContent = availableCoin;
     }
     else{
-        alert('you have less than 20 coin')
+        alert('not enough coins')
+    }
+})
+document.getElementById('call-btn2').addEventListener('click', function(){
+    
+    if(availableCoin >= 20){
+        alert("Calling Police 999");
+        availableCoin = availableCoin - 20;
+        coinCount.textContent = availableCoin;
+    }
+    else{
+        alert('not enough coins')
+    }
+})
+document.getElementById('call-btn3').addEventListener('click', function(){
+    
+    if(availableCoin >= 20){
+        alert("Calling Fire Service 999");
+        availableCoin = availableCoin - 20;
+        coinCount.textContent = availableCoin;
+    }
+    else{
+        alert('not enough coins')
+    }
+})
+document.getElementById('call-btn4').addEventListener('click', function(){
+    
+    if(availableCoin >= 20){
+        alert("Calling Ambulance 1994-999999");
+        availableCoin = availableCoin - 20;
+        coinCount.textContent = availableCoin;
+    }
+    else{
+        alert('not enough coins')
+    }
+})
+document.getElementById('call-btn5').addEventListener('click', function(){
+    
+    if(availableCoin >= 20){
+        alert("calling Women & Child Help 109");
+        availableCoin = availableCoin - 20;
+        coinCount.textContent = availableCoin;
+    }
+    else{
+        alert('not enough coins')
+    }
+})
+document.getElementById('call-btn6').addEventListener('click', function(){
+    
+    if(availableCoin >= 20){
+        alert("Calling Anti Corruption Commission 106");
+        availableCoin = availableCoin - 20;
+        coinCount.textContent = availableCoin;
+    }
+    else{
+        alert('not enough coins')
+    }
+})
+document.getElementById('call-btn7').addEventListener('click', function(){
+    
+    if(availableCoin >= 20){
+        alert("Calling Electricity Outage 16216");
+        availableCoin = availableCoin - 20;
+        coinCount.textContent = availableCoin;
+    }
+    else{
+        alert('not enough coins')
+    }
+})
+
+document.getElementById('call-btn8').addEventListener('click', function(){
+    
+    if(availableCoin >= 20){
+        alert("Calling BRAC 16445");
+        availableCoin = availableCoin - 20;
+        coinCount.textContent = availableCoin;
+    }
+    else{
+        alert('not enough coins')
+    }
+})
+
+document.getElementById('call-btn9').addEventListener('click', function(){
+    
+    if(availableCoin >= 20){
+        alert("Calling Bangladesh Railway 163");
+        availableCoin = availableCoin - 20;
+        coinCount.textContent = availableCoin;
+    }
+    else{
+        alert('not enough coins')
     }
 })
